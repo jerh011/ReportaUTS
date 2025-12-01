e547b522c5059a1b4af72e2bf26aa67a1f468239
Jerh011 <jeelrohe@gmail.com>
2025-12-01 08:30:01 -0700

    ajuste a los reportes

--- env.ts
+++ env.ts
@@ -1,4 +1,5 @@
 export const env = {
-  VITE_API_URL: "http://192.168.1.100:5000/api",
-  VITE_API_IMAGE: "http://192.168.1.100:5000",
+  VITE_API_URL: "https://reportauts-api-ahqs.onrender.com/api",
+  VITE_API_IMAGE: "https://reportauts-api-ahqs.onrender.com",
+  
 };
